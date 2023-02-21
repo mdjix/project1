c61c70f4b3e54d7bff8e7ae07dbee0d3f50ff491
Alekseeva332 <84273151+Alekseeva332@users.noreply.github.com>
2023-02-21 22:15:56 +0300

    euclidean distance

--- lesson_9/task_4.py
+++ lesson_9/task_4.py
@@ -2,10 +2,12 @@ import math
 
 
 def find_distance(*args):
-    d = 0
-    for i in args:
-        d += (i[1]-i[0])**2
-    distance = math.sqrt(d)
+    length = 0
+    for index, point in enumerate(args[:-1]):
+        x1, y1 = point
+        x2, y2 = args[index+1]
+        length += (x2 - x1)**2 + (y2 - y1)**2
+    distance = math.sqrt(length)
     return distance
 
 
