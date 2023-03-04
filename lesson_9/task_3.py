838db444beabd622500076f21ceda3736021a318
Alekseeva332 <84273151+Alekseeva332@users.noreply.github.com>
2023-03-04 15:20:40 +0300

    letter counter

--- lesson_9/task_3.py
+++ lesson_9/task_3.py
@@ -4,8 +4,7 @@ def letter_counter(file):
     with open(file, 'r') as file:
         for line in file:
             num_of_letters += line.count(letter.lower())
-    file.close()
-    return print(f'Letter {letter} appears {num_of_letters} times in the file.')
+    return print(f'Letter {letter} appears {num_of_letters} times in the file')
 
 
 letter_counter('story.txt')
